c2a45516405ec2a08734c5aeb41bd30cdf6fc7d1
Aleksej_Sivko <108893186+Chikago13@users.noreply.github.com>
2023-09-03 15:33:18 +0300

    Update task5.py

--- Execise/task5.py
+++ Execise/task5.py
@@ -1,1 +1,19 @@
+from random import randint
+
+def list_dict(lenlist: int):
+    d = []
+    list_name = ['Alex', 'Tim', 'Alen', 'Olga', 'Nik', 'Svet']
+    for i in range(lenlist):
+        d.append({'name':f'{list_name[randint(0, len(list_name)-1)]}', 'number': f'{randint(50, 100)}'})
+    return d
+
+def new_dict(list_val, k):
+    if k.isdigit():
+        return list(filter(lambda x:  x['number']==k, list_val))
+    else:
+        return list(filter(lambda x:  x['name']==k, list_val))
+
+res = list_dict(10)
+print(res)
+print(new_dict(res, '92'))
 
