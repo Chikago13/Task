3f91a4fca0c72861fb91637636c917619b64d408
Chikago13 <sivko2201@gmail.com>
2023-09-06 20:57:29 +0300

    homework7 and task6

---
+++ Homework_1/homework7.py
@@ -0,0 +1,32 @@
+# Создать класс Human c атрибутами name, surname, birth_year и двумя методами full_name(), который будет  возвращать полное имя: Name Surname и get_age(), 
+# возвращающий возраст, созданного экземпляра 
+
+from datetime import datetime
+
+
+class Human:
+    name: str
+    surname:str
+    birth_year: int
+    
+    def __init__(self, name, surname, birth_year):
+        self.name = name
+        self.surname = surname
+        self.birth_year = birth_year
+
+
+    def full_name(self):
+        return f'Имя: {self.name} Фамилия: {self.surname}'
+    
+    def get_age(self):
+        return f'Год рождения: {self.birth_year}'
+
+    # def humah_age(self):
+    #     return f' Возраст человека: {datetime.now().year-self.birth_year}'
+
+
+human_1 = Human('Alex', 'Volkov', 1988)
+
+print(human_1.full_name())
+print(human_1.get_age())
+# print(human_1.humah_age())
