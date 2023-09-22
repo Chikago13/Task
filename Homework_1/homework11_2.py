f1dc665705c6e6409687ad96f52f84592d63d78e
Chikago13 <sivko2201@gmail.com>
2023-09-22 22:06:55 +0300

    completion of homeworl11_2

--- Homework_1/homework11_2.py
+++ Homework_1/homework11_2.py
@@ -3,15 +3,20 @@
 # у класса "Автобус". Метод "прицепить вагон" и атрибут "вагон-ресторан" у класса "Поезд".
 # В каждом классе реализуйте счетчик экземпляров. Используйте декоратор @classmethod. Создайте функцию, генерирующую заданное кол-во экземпляров класса поезд/автобус и 
 # возвращающую список этих экземпляров. Создайте функцию, которая выполнит сортировку экземпляров по переданному параметру "скорость", "вместимость". 
-# from operator import attrgetter
+from random import randint, choice
+
+COMMANDS_NAMES = ['A1', 'A2', 'B1', 'B2', 'B3', 'G1', 'G2', 'G3', 'T1', 'T2', 'T3', 'R1', 'R2', 'R3']
+
 
 class Transport:
     count = 0
+
 
     def __init__(self, speed, capacity):
         self.speed = speed
         self.capacity = capacity
         Transport.count +=1
+
 
     def go(self):
         return f'go'
@@ -19,8 +24,22 @@ class Transport:
     def stop(self):
         return f'stoped'
     
-    # def __str__(self) -> str:
-    #     return f' {self.speed} {self.capacity}'
+
+    @classmethod
+    def generate_instances(cls, num):
+        instances = []
+        for _ in range(num):
+            speed = randint(20, 100)
+            capacity = randint(1, 250)
+            instance = cls(speed, capacity)
+            instances.append(instance)
+        return instances
+    
+    def __str__(self) -> str:
+        return f' {self.speed} {self.capacity}'
+    
+    def __repr__(self) -> str:
+        return f' {self.speed} {self.capacity}'
     
 class Bus(Transport):
     count = 0
@@ -30,8 +49,21 @@ class Bus(Transport):
         self.route_number = route_number
         Bus.count +=1
 
+
     def open_door(self):
         return f'Doors open'
+    
+
+    @classmethod
+    def generate_instances(cls, num):
+        instances = []
+        for _ in range(num):
+            speed = randint(20, 100)
+            capacity = randint(1, 250)
+            route_number = choice(COMMANDS_NAMES)
+            instance = cls(speed, capacity, route_number)
+            instances.append(instance)
+        return instances
     
     def __str__(self) -> str:
         return f' {self.speed} {self.capacity} {self.route_number}'
@@ -53,6 +85,20 @@ class Train(Transport):
     def attach_wagon(self):
         return f'Wagon attached.'
     
+    
+
+    @classmethod
+    def generate_instances(cls, num):
+        instances = []
+        for _ in range(num):
+            speed = randint(20, 100)
+            capacity = randint(1, 250)
+            has_restoran = choice([True, False])
+            instance = cls(speed, capacity, has_restoran)
+            instances.append(instance)
+        return instances
+    
+
     def __str__(self) -> str:
         return f' {self.speed} {self.capacity} {self.has_restoran}'
     
@@ -60,11 +106,11 @@ class Train(Transport):
         return f' {self.speed} {self.capacity} {self.has_restoran}'
     
 
-def generate_instances(cls, num):
-    instances = []
-    for _ in range(num):
-        instances.append(cls)
-    return instances
+# def generate_instances(cls, num):
+#     instances = []
+#     for _ in range(num):
+#         instances.append(cls)
+#     return instances
 
 def sorting_instances_speed(instances):
     return sorted(instances, key=lambda x: x.speed)
@@ -75,13 +121,9 @@ def sorting_instances_capacity(instances):
 
 
 
-bus1 = Bus(90, 50, "A1")
-bus2 = Bus(50, 40, "B2")
-train1 = Train(60, 500, True)
-train2 = Train(80, 400, False)
 
-bus = generate_instances(bus1, 5)
-train = generate_instances(train1, 3)
+bus = Bus.generate_instances(5)
+train = Train.generate_instances(3)
 
 sort_speed = sorting_instances_speed(bus + train)
 sort_capacity = sorting_instances_capacity(bus + train)
@@ -89,5 +131,9 @@ sort_capacity = sorting_instances_capacity(bus + train)
 print(sort_speed)
 print(sort_capacity)
 
+
+
+# transp1 = Transport.generate_instances(3)
+# print(transp1)
 
 
