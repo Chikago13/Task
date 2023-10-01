cc153bb0084a8deda38fec67a34be8570e31eefe
Aleksej_Sivko <108893186+Chikago13@users.noreply.github.com>
2023-10-01 13:15:13 +0300

    Update homework13.py

--- Homework_1/homework13.py
+++ Homework_1/homework13.py
@@ -11,9 +11,11 @@ class BankAccount:
         self.password = password
         self.balance = 0
         self.is_frozen = False
+        self.is_logged = False
 
     def login(self, card_number, password):
         if card_number == self.card_number and password == self.password:
+            self.is_logged = True
             return "Вход выполнен успешно!"
         else:
             return "Неправильный номер карты или пароль!"
@@ -24,41 +26,59 @@ class BankAccount:
         return "Открыт новый счет."
 
     def deposit(self, amount):
-        if self.is_frozen:
-            return "Счет заморожен. Невозможно совершить операцию."
-        else:
+        if not self.is_logged:
+            return f"Необходимо выполнить вход."
+        if not self.is_frozen:
             self.balance += amount
             return f"Счет пополнен на {amount}."
+        else:
+            return "Счет заморожен. Невозможно совершить операцию."
+
 
     def withdraw(self, amount):
-        if self.is_frozen:
-            return "Счет заморожен. Невозможно совершить операцию."
-        elif self.balance < amount:
-            return "Недостаточно средств на счете."
-        else:
+        if not self.is_logged:
+            return f"Необходимо выполнить вход."
+        if not self.is_frozen:
+            if self.balance < amount:
+                return "Недостаточно средств на счете."
             self.balance -= amount
             return f"Снято со счета: {amount}."
+        else:
+            return "Счет заморожен. Невозможно совершить операцию."
+
 
     def transfer(self, amount, recipient):
-        if self.is_frozen:
-            return "Счет заморожен. Невозможно совершить операцию."
-        elif self.balance < amount:
-            return "Недостаточно средств на счете."
-        else:
+        if not self.is_logged:
+            return f"Необходимо выполнить вход."
+        if not self.is_frozen:
+            if self.balance < amount:
+                return "Недостаточно средств на счете."
             self.balance -= amount
             recipient.deposit(amount)
             return  f"Перевод {amount} выполнен успешно."
+        else:
+            return "Счет заморожен. Невозможно совершить операцию."
 
     def check_balance(self):
-        return f"Баланс счета: {self.balance}."
+        if not self.is_logged:
+            return f"Необходимо выполнить вход."
+        else:
+            return f"Баланс счета: {self.balance}."
 
     def freeze_account(self):
-        self.is_frozen = True
-        return "Счет заморожен."
+        if not self.is_logged:
+            return f"Необходимо выполнить вход."
+        else:
+            self.is_frozen = True
+            return f"Счет заморожен."
 
     def change_password(self, new_password):
-        self.password = new_password
-        return "Пароль успешно изменен."
+        if not self.is_logged:
+            return f"Необходимо выполнить вход."
+        else:
+            self.password = new_password
+            return f"Пароль успешно изменен."
+
 
 
 account = BankAccount("1234567890", "password")
@@ -73,4 +93,3 @@ print(account.transfer(300, recipient))
 print(account.check_balance())
 print(recipient.check_balance())
 print(account.change_password("new_password"))
-
